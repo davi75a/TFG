5998b510ea4dbf7a09eaf50b97e63c1aa5d21be8
David <David@DESKTOP-ICMAKU9>
2023-05-17 18:32:56 +0200

    Agregar archivos de proyecto.

---
+++ TFG/Conexion.cs
@@ -0,0 +1,37 @@
+﻿
+using MySql.Data.MySqlClient;
+
+namespace TFG
+{
+    internal class Conexion
+    {
+        private MySqlConnection conexion;
+        private string server = "localhost";
+        private string database = "emporium_bd";
+        private string user = "root";
+        private string password = "admin";
+        private string cadanaConexion;
+        public static Conexion mConexion;
+
+
+        public Conexion()
+        {
+            cadanaConexion = "Database=" + database +
+                "; DataSource=" + server +
+                "; User Id= " + user +
+                "; Password= " + password;
+
+        }
+
+        public MySqlConnection getConxion()
+        {
+            if (conexion == null)
+            {
+                conexion = new MySqlConnection(cadanaConexion);
+                conexion.Open();
+            }
+
+            return conexion;
+        }
+    }
+}
